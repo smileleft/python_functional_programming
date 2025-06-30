17136d2785666f69c3594b7feb5c50e73762257a
smileleft <spyt77@gmail.com>
2025-06-30 16:55:49 +0900

    feature:list comprehension for filter

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
+def is_even(x):
+    return (x % 2) == 0
+
 def upper(s):
     return s.upper()
 
+
 def main():
-    print(list(map(upper, ['sentence', 'fragment'])))
+    print(list(x for x in range(10) if is_even(x)))
 
 
 if __name__ == '__main__':
